882648a6b24404c910c0942825e52d4ce02b3993
Xiaoming Sun <rsun2014@gmail.com>
2018-03-25 13:17:43 -0700

    Reorg GET tests by data provider

--- src/main/java/pers/xiaoming/hibernate/command/sql/GetTopTen.java
+++ src/main/java/pers/xiaoming/hibernate/command/sql/GetTopTen.java
@@ -1,25 +1,21 @@
-package pers.xiaoming.hibernate.command.qbc;
+package pers.xiaoming.hibernate.command.sql;
 
-import org.hibernate.Criteria;
 import org.hibernate.Session;
-import org.hibernate.criterion.Order;
-import org.hibernate.criterion.Restrictions;
 import pers.xiaoming.hibernate.command.GetTopTenStudents;
 import pers.xiaoming.hibernate.entity.Student;
 
 import java.util.List;
 
-public class Sort implements GetTopTenStudents {
+public class GetTopTen implements GetTopTenStudents {
+    private final static String SQL_QUERY = "SELECT t_id, t_name, t_age, t_score FROM t_student ORDER BY t_score DESC LIMIT 10;";
+
     @SuppressWarnings("unchecked")
     public List<Student> get(Session session) {
         try {
             session.beginTransaction();
 
-            Criteria criteria = session.createCriteria(Student.class);
-            criteria.addOrder(Order.desc("score"));
-            criteria.add(Restrictions.sizeEq("t_id", 10));
-
-            List<Student> list = criteria.list();
+            List<Student> list = session.createSQLQuery(SQL_QUERY)
+                    .addEntity(Student.class).list();
 
             session.getTransaction().commit();
             return list;
---
+++ src/test/java/pers/xiaoming/hibernate/query/GetQueryTest.java
@@ -0,0 +1,52 @@
+package pers.xiaoming.hibernate.query;
+
+import org.junit.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import pers.xiaoming.hibernate.command.GetStudent;
+import pers.xiaoming.hibernate.command.GetTopTenStudents;
+import pers.xiaoming.hibernate.command.hql.GetTopTen;
+import pers.xiaoming.hibernate.entity.Student;
+import pers.xiaoming.hibernate.session_factory.Server;
+
+import java.util.List;
+
+public class GetQueryTest {
+    private List<Integer> ids;
+
+    @BeforeClass
+    public void setUp() {
+        ids = DataProcessor.getIds();
+    }
+
+    @Test(dataProvider = "get_student")
+    public void testGetStudent(GetStudent getStudent) {
+        Student student = getStudent.get(Server.getSession(), ids.get(0));
+        Assert.assertTrue(DataProcessor.validateStudent(student));
+    }
+
+    @DataProvider(name = "get_student")
+    public Object[][] getStudentDataProvider() {
+        return new Object[][] {
+                {new pers.xiaoming.hibernate.command.sql.BasicGet()},
+                {new pers.xiaoming.hibernate.command.hql.BasicGet()},
+                {new pers.xiaoming.hibernate.command.qbc.BasicGet()},
+        };
+    }
+
+    @Test(dataProvider = "get_topten_student")
+    public void testGetTopTenStudents(GetTopTenStudents getStudents) {
+        List<Student> students = getStudents.get(Server.getSession());
+        Assert.assertEquals(10, students.size());
+    }
+
+    @DataProvider(name = "get_topten_student")
+    public Object[][] getTopTenStudentDataProvider() {
+        return new Object[][] {
+                {new pers.xiaoming.hibernate.command.sql.GetTopTen()},
+                {new pers.xiaoming.hibernate.command.hql.GetTopTen()},
+                {new pers.xiaoming.hibernate.command.qbc.GetTopTen()},
+        };
+    }
+}
