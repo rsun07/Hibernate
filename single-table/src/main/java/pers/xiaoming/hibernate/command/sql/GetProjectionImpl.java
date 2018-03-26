154bb8795954e7ca03201f1022a97d08d78848e1
Xiaoming Sun <rsun2014@gmail.com>
2018-03-26 15:08:47 -0700

    GetProjection sql impl

---
+++ single-table/src/main/java/pers/xiaoming/hibernate/command/sql/GetProjectionImpl.java
@@ -0,0 +1,33 @@
+package pers.xiaoming.hibernate.command.sql;
+
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
+import pers.xiaoming.hibernate.command.get_interface.GetProjection;
+import pers.xiaoming.hibernate.entity.Student;
+
+import java.util.List;
+
+public class GetProjectionImpl implements GetProjection {
+
+    private final static String QUERY = "SELECT t_age, t_score FROM t_student WHERE t_age > ? LIMIT 50;";
+
+    @SuppressWarnings("unchecked")
+    public List<Student> get(Session session, int age) throws Exception {
+        try {
+            session.beginTransaction();
+
+            SQLQuery query = session.createSQLQuery(QUERY);
+            query.setInteger(0, age);
+            query.addEntity(Student.class);
+            List<Student> list = query.list();
+
+            session.getTransaction().commit();
+
+            return list;
+
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        }
+    }
+}
