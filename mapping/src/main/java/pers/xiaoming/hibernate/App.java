28bc7d1741ab2f6301f483d72cfcb0206f0ab9f5
Xiaoming Sun <rsun2014@gmail.com>
2018-03-25 19:39:35 -0700

    Introduce mapping sub module

---
+++ mapping/src/main/java/pers/xiaoming/hibernate/App.java
@@ -0,0 +1,13 @@
+package pers.xiaoming.hibernate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
