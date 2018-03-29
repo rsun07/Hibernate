5936f386017add78003a6eee1b8f913008b2be0e
Xiaoming Sun <rsun2014@gmail.com>
2018-03-29 15:43:58 -0700

    Add distinct to HQL query for left fetch join

--- table-join/src/main/java/pers/xiaoming/hibernate/command/left_fetch_join/Hql.java
+++ table-join/src/main/java/pers/xiaoming/hibernate/command/left_fetch_join/Hql.java
@@ -1,5 +1,6 @@
 package pers.xiaoming.hibernate.command.left_fetch_join;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 import pers.xiaoming.hibernate.command.GetCity;
 import pers.xiaoming.hibernate.entity.City;
@@ -10,7 +11,8 @@ import java.util.List;
 public class Hql implements GetCity {
 
     // In grammar, just replace 'join' to 'fetch join'
-    private static final String QUERY = "from City c left outer join fetch c.residents where c.id = :city_id";
+    // add a distinct to avoid duplication
+    private static final String QUERY = "select distinct c from City c left outer join fetch c.residents where c.id = :city_id";
 
     @Override
     @SuppressWarnings("unchecked")
@@ -21,7 +23,16 @@ public class Hql implements GetCity {
             int dbIndex = id + 1;
 
             // Fetch join already encapsulate the City and also the List<Person> residents in the City Object
-            City city = (City) session.createQuery(QUERY).setInteger("city_id", dbIndex).uniqueResult();
+            Query query = session.createQuery(QUERY);
+            query.setInteger("city_id", dbIndex);
+
+            // If get the list here rather than call the uniqueResult() method
+            // There will be multiple duplicate City instances
+            // For example, if the city has 5 residents Person, there will be five City instances
+            // To avoid duplication, just add a 'distinct' key word in the query
+            List<Object> list = query.list();
+
+            City city = (City) query.uniqueResult();
 
             session.getTransaction().commit();
 
