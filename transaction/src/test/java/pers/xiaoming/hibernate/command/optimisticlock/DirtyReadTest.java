6a2dac6083a545a007096c9d5cb7e08994b4cd26
Xiaoming Sun <rsun2014@gmail.com>
2018-04-01 10:17:15 -0700

    Introduce logback into transaction

---
+++ transaction/src/test/java/pers/xiaoming/hibernate/command/optimisticlock/DirtyReadTest.java
@@ -0,0 +1,75 @@
+package pers.xiaoming.hibernate.command.optimisticlock;
+
+import org.hibernate.Session;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pers.xiaoming.hibernate.entity.StudentVersion;
+import pers.xiaoming.hibernate.session_factory.SessionManager;
+
+public class DirtyReadTest {
+    private static final int TEST_ID = 1;
+    private static final Logger logger = LoggerFactory.getLogger(DirtyReadTest.class.getName());
+
+    @Test
+    public void test() {
+        Thread readThread = new Thread(this::readTransaction);
+        readThread.setName("ReadThread");
+        readThread.start();
+
+        Thread updateThread = new Thread(this::updateTransaction);
+        updateThread.setName("UpdateThread");
+        updateThread.start();
+    }
+
+    private void readTransaction() {
+        logger.info("read thread start");
+
+        Session session = SessionManager.getSession();
+
+        session.beginTransaction();
+
+        StudentVersion before = session.get(StudentVersion.class, TEST_ID);
+
+        logger.info("read thread get for the first time and sleep");
+
+        sleep(200);
+
+        StudentVersion after = session.get(StudentVersion.class, TEST_ID);
+
+        logger.info("read thread get for the second time and compare");
+
+        Assert.assertEquals(after, before);
+    }
+
+    private void updateTransaction() {
+        logger.info("update thread start and sleep");
+
+        sleep(100);
+
+        Session session = SessionManager.getSession();
+
+        session.beginTransaction();
+
+        StudentVersion student = session.get(StudentVersion.class, TEST_ID);
+
+        student.setName("Mike");
+
+        session.update(student);
+
+        logger.info("update thread update and sleep");
+
+        sleep(200);
+
+        session.getTransaction().rollback();
+    }
+
+    private void sleep(int milliSecs) {
+        try {
+            Thread.sleep(milliSecs);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
